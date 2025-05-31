a883ef07a3733cfb7af0ecb2813137507d717579
Soufiane MEFTAH <s.mef2703@gmail.com>
2025-05-31 20:53:23 +0200

    Ajoute les champs liés aux offres spéciales dans le modèle Cart

--- models/cart.model.js
+++ models/cart.model.js
@@ -9,14 +9,38 @@ module.exports = (sequelize, DataTypes) => {
         min: 1,
       },
     },
+    specialOfferId: {
+      type: DataTypes.INTEGER,
+      allowNull: true, // Peut être nul s’il n’y a pas d’offre
+    },
+    discountAmount: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      defaultValue: 0,
+    },
+    discountedPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
   }, {
     tableName: 'carts',
     timestamps: true,
   });
 
   Cart.associate = (models) => {
-    Cart.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
-    Cart.belongsTo(models.Product, { foreignKey: 'productId', as: 'product' });
+    Cart.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user'
+    });
+    Cart.belongsTo(models.Product, {
+      foreignKey: 'productId',
+      as: 'product'
+    });
+
+    Cart.belongsTo(models.SpecialOffer, {
+      foreignKey: 'specialOfferId',
+      as: 'specialOffer'
+    });
   };
 
   return Cart;
